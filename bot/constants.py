92b602563f988eb0360d344137a18e78530c852e
Immijimmi <immijimmi1@gmail.com>
2020-04-23 14:05:23 +0100

    Amended default timeout_duration to 1s

--- bot/constants.py
+++ bot/constants.py
@@ -4,7 +4,7 @@ class KeyQueryFactories:
     dynamic_key = KeyQueryFactory(lambda sub_state, user_id: user_id)
 
 class Defaults:
-    timeout_duration = 5
+    timeout_duration = 1
 
 class MessageFormats:
     welcome_header = "**Welcome back!**"
