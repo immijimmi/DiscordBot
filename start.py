28d5583589f502e172bc75ace5fc4a2eb0ee80b5
Immijimmi <immijimmi1@gmail.com>
2020-04-28 16:07:10 +0100

    Added catchall for fatal errors

--- start.py
+++ start.py
@@ -1,6 +1,19 @@
+import logging
+
 from bot import Bot
 from bot.plugins import Watchlist, HelpCommand
 
 from config import Config
 
-Bot(Config.token, plugins=[Watchlist, HelpCommand], log_destination_ids=Config.log_destination_ids)
+try:
+    bot = Bot(
+        Config.token,
+        plugins=[Watchlist, HelpCommand],
+        log_destination_ids=Config.log_destination_ids
+        )
+
+except Exception as ex:
+    print("An unexpected error has occurred.")
+    logging.exception(ex)
+
+    print("\n" + "Press Enter to quit: ")
