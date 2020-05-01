594f14d6abe210f011269edc98208b52f5f7f879
Immijimmi <immijimmi1@gmail.com>
2020-05-01 02:37:16 +0100

    Changed private variables to protected in EventTimeout

--- bot/classes/eventTimeout.py
+++ bot/classes/eventTimeout.py
@@ -2,13 +2,17 @@ from datetime import datetime, timedelta
 
 class EventTimeout:
     def __init__(self, key, timeout_duration):
-        self.__key = key
-        self.__duration = timeout_duration  # Should be an instance of TimeoutDuration class
+        self._key = key
+        self._duration = timeout_duration  # Should be an instance of TimeoutDuration class
 
         self.reset()
 
+    @property
+    def start(self):
+        return self._start
+
     def is_expired(self):
-        return (self.__start + timedelta(seconds=self.__duration.seconds)) <= datetime.now()
+        return (self._start + timedelta(seconds=self._duration.seconds)) <= datetime.now()
 
     def reset(self):
-        self.__start = datetime.now()
+        self._start = datetime.now()
