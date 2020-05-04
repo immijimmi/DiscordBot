c77b8116519449a72e23f530e86e67b432eb869b
Immijimmi <immijimmi1@gmail.com>
2020-05-04 02:26:29 +0100

    Moved SymbolLookup to plugins level

--- bot/plugins/constants.py
+++ bot/plugins/constants.py
@@ -1,7 +1,10 @@
+from discord.state import Status
+
 from ..classes.timeoutDuration import TimeoutDuration
 
 class MessageFormats:
     cannot_find_user_identifier = "Unable to find a user based on the name: `{0}`"
+    cannot_find_user_placeholder = "*unable to find user*"
     cannot_parse_timeout_string = "Unable to get a timeout duration from the provided value: `{0}`\nTimeout duration must be a number of seconds between 1 and " + str(TimeoutDuration.max_seconds) + "."
     invalid_arguments = "Invalid command arguments: `{0}`"
 
@@ -13,3 +16,13 @@ class MessageFormats:
 
 class Arguments:
     nickname_separator = " as: "
+
+class SymbolLookup:
+    status = {
+        Status.online: ":green_circle:",
+        Status.idle: ":orange_circle:",
+        Status.dnd: ":red_circle:",
+        Status.offline: ":white_circle:",
+        Status.invisible: ":black_circle:",
+        "unknown": ":grey_question:"
+        }
--- bot/plugins/watchlist/constants.py
+++ bot/plugins/watchlist/constants.py
@@ -5,16 +5,7 @@ class MessageFormats:
     watchlist_user_removed = "{0} has been removed from your watchlist."
     watchlist_title_private = "**Your watchlist:**"
 
-    status_order = [Status.online, Status.dnd, Status.idle, Status.offline, Status.invisible]
-
-class SymbolLookup:
-    status = {
-        Status.online: ":green_circle:",
-        Status.idle: ":orange_circle:",
-        Status.dnd: ":red_circle:",
-        Status.offline: ":white_circle:",
-        Status.invisible: ":black_circle:"
-        }
+    status_order = [Status.online, Status.dnd, Status.idle, Status.offline, Status.invisible, "unknown"]
 
 class EventKeys:
     watchlist_alerts = "user_watchlist_alert|{0}|{1}"
