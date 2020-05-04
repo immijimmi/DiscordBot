5cc2ce1e09bdbad8ea84baa8b71d4706aec8be4a
Immijimmi <immijimmi1@gmail.com>
2020-05-05 00:17:58 +0100

    Added logging config

--- config.py
+++ config.py
@@ -1,4 +1,10 @@
 class Config:
     token = ""
 
+    logging = {
+        "filename":"bot.log",
+        "level": logging.ERROR,
+        "format": "%(asctime)s|%(levelname)s:%(message)s"
+    }
     log_destination_ids = []
+    
