872bcf163caa2f4dd7bc0b2c66cd7c1011d3233a
Immijimmi <immijimmi1@gmail.com>
2020-06-08 10:54:26 +0100

    More minor changes to stylisation of !mc

--- bot/plugins/kiko/constants.py
+++ bot/plugins/kiko/constants.py
@@ -3,16 +3,14 @@ class PermissionsValues:
     tags = [tag__kiko]
 
 class MessageFormats:
-    mc_setup__server_ip = """
-Install
-- Go to https://tlauncher.org/en/
-- Click "DOWNLOAD TL" - select mac/windows/linux
-- Install TLauncher and java if you are asked (create desktop item or shortcut and launcher once finished)
-
-Download Mods
--  Click the button towards the bottom right that says TL MODs
--  Search for "Tekkit Classic Reloaded" at the top - click and install
--  Enter name at the bottom left and play
-
-Enter the server ip: **`{0}`**
-"""
+    mc_setup__server_ip = "**Install**" + "\n"
+    mc_setup__server_ip += "- Go to https://tlauncher.org/en/" + "\n"
+    mc_setup__server_ip += "- Click 'DOWNLOAD TL' - select mac/windows/linux" + "\n"
+    mc_setup__server_ip += "- Install TLauncher and java if you are asked (create desktop item or shortcut and launcher once finished)" + "\n"
+    mc_setup__server_ip += "\n"
+    mc_setup__server_ip += "**Download Mods**" + "\n"
+    mc_setup__server_ip += "-  Click the button towards the bottom right that says 'TL MODS'" + "\n"
+    mc_setup__server_ip += "-  Search for 'Tekkit Classic Reloaded' at the top - click and install" + "\n"
+    mc_setup__server_ip += "-  Enter name at the bottom left and play" + "\n"
+    mc_setup__server_ip += "\n"
+    mc_setup__server_ip += "Enter the server ip: **`{0}`**"
